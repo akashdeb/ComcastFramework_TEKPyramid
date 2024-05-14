cef6e31c56a716833fb34cfe82d6305cd57d4f73
NEHA ZUTTI <NEHA ZUTTI@LAPTOP-UHSOF6P6>
2024-05-14 10:29:01 +0530

    changes done

---
+++ ComcastCRMGUIFramework/src/test/java/com/crm/github/NewBranchTest.java
@@ -0,0 +1,12 @@
+package com.crm.github;
+
+import org.testng.annotations.Test;
+
+public class NewBranchTest {
+
+	@Test
+	public void newTest()
+	{
+		System.out.println("--- practice test ---");
+	}
+}
